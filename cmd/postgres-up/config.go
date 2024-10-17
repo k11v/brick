632eafddb183afa6bc8eb9a12fe3d26eda1be86d
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-10-17 15:17:18 +0300

    Add postgres-up, postgresutil

---
+++ cmd/postgres-up/config.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/caarlos0/env/v11"
+	"github.com/k11v/brick/internal/postgresutil"
+)
+
+// config holds the application configuration.
+type config struct {
+	Postgres postgresutil.Config `envPrefix:"BRICK_POSTGRES_"`
+}
+
+// parseConfig parses the application configuration from the environment variables.
+func parseConfig(environ []string) (*config, error) {
+	var cfg config
+
+	err := env.ParseWithOptions(&cfg, env.Options{
+		Environment: env.ToMap(environ),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
