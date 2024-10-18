e9f58b1472e5334c05418fe37ec3ad39a3151de4
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-10-18 17:48:01 +0300

    Add build.Service.DeleteCache

--- internal/build/build.go
+++ internal/build/build.go
@@ -6,12 +6,17 @@ import (
 	"github.com/google/uuid"
 )
 
-type Service struct {}
+// cache can be invalidated manually using cache key.
+// cache can be invalidated automatically when too much time passed.
+// cache can be invalidated automatically when too much space used.
+// when cache is invalidated, it is deleted.
+
+type Service struct{}
 
 type CreateParams struct {
-	UserID uuid.UUID
-	DocumentFiles map[string][]byte
-	CacheKey uuid.UUID
+	UserID         uuid.UUID
+	DocumentFiles  map[string][]byte
+	CacheKey       uuid.UUID
 	IdempotencyKey uuid.UUID
 }
 
@@ -21,9 +26,15 @@ type CreateResult struct {
 
 func (s *Service) Create(createParams *CreateParams) (*CreateResult, error) {
 	// POST /builds
+	//
+	// IdempotencyKey will be read from X-Idempotency-Key header field.
+	// Draft for idempotency key exists but it is not accepted yet.
+	// See https://datatracker.ietf.org/doc/draft-ietf-httpapi-idempotency-key-header/.
+	//
 	// check limits for user ID || return error
 	// check cache key for user ID || return error
-	// insert into builds (new UUID, new date, user ID, idempotency key, status, document files (T), cache key (T)) || return error
+	// select cached files from caches using cache key
+	// insert into builds (new UUID, new date, user ID, idempotency key, status, document files (T), cached files (T)) || return error
 	// return build ID
 	panic("not implemented")
 }
@@ -33,14 +44,27 @@ type LimitsParams struct {
 }
 
 type LimitsResult struct {
-	BuildsUsed int
+	BuildsUsed    int
 	BuildsAllowed int
-	ResetsAt time.Time
+	ResetsAt      time.Time
 }
 
 func (s *Service) Limits(limitsParams *LimitsParams) (*LimitsResult, error) {
 	// GET /builds/limits
 	// get limits for user ID
 	// return builds used, builds allowed and resets at
+	panic("not implemented")
+}
+
+type DeleteCacheParams struct {
+	UserID   uuid.UUID
+	CacheKey uuid.UUID
+}
+
+func (s *Service) DeleteCache(deleteCacheParams *DeleteCacheParams) error {
+	// DELETE /caches/{cache-key}
+	// check cache key for user ID || return error
+	// delete from caches using cache key || return error (endpoint should return 404 if not found)
+	// return nil (endpoint should return 201 if found and deleted)
 	panic("not implemented")
 }
