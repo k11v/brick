165f7c246ff6554691652c6d94214027b02b9e6d
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-10-29 03:40:45 +0300

    Add fake database
    Current test fails because it gets a random UUID when it wants a
    hardcoded.
    
    The fake database's transaction isolation level is like serializable but
    stricter: only one transaction runs at the same time.

--- internal/build/build_test.go
+++ internal/build/build_test.go
@@ -3,39 +3,169 @@ package build
 import (
 	"errors"
 	"reflect"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/google/uuid"
 )
 
-type MockDatabase struct {
-	TransactWithUserFunc func(userID uuid.UUID) (database Database, commit func() error, rollback func() error, err error)
-	GetBuildCountFunc    func(userID uuid.UUID, startTime time.Time, endTime time.Time) (int, error)
-	CreateBuildFunc      func(contextToken string, documentFiles map[string][]byte, idempotencyKey uuid.UUID, userID uuid.UUID) (*Build, error)
+type FakeDatabaseBuild struct {
+	DatabaseBuild  DatabaseBuild
+	ContextToken   string
+	DocumentFiles  map[string][]byte
+	IdempotencyKey uuid.UUID
+	UserID         uuid.UUID
+	CreatedAt      time.Time
 }
 
-func (m *MockDatabase) CreateBuild(contextToken string, documentFiles map[string][]byte, idempotencyKey uuid.UUID, userID uuid.UUID) (*Build, error) {
-	return m.CreateBuildFunc(contextToken, documentFiles, idempotencyKey, userID)
+type FakeDatabase struct {
+	builds       []*FakeDatabaseBuild
+	mu           sync.Mutex
+	muFromUserID map[uuid.UUID]*sync.Mutex
 }
 
-func (m *MockDatabase) GetBuildCount(userID uuid.UUID, startTime time.Time, endTime time.Time) (int, error) {
-	return m.GetBuildCountFunc(userID, startTime, endTime)
+func (d *FakeDatabase) CreateBuild(params *DatabaseCreateBuildParams) (*DatabaseBuild, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	b := &FakeDatabaseBuild{
+		DatabaseBuild: DatabaseBuild{
+			Done:             false,
+			Error:            nil,
+			ID:               uuid.New(),
+			NextContextToken: "",
+			OutputFile:       nil,
+		},
+		ContextToken:   params.ContextToken,
+		DocumentFiles:  params.DocumentFiles,
+		IdempotencyKey: params.IdempotencyKey,
+		UserID:         params.UserID,
+		CreatedAt:      time.Now().UTC(),
+	}
+	d.builds = append(d.builds, b)
+	return &b.DatabaseBuild, nil
 }
 
-func (m *MockDatabase) TransactWithUser(userID uuid.UUID) (database Database, commit func() error, rollback func() error, err error) {
-	return m.TransactWithUserFunc(userID)
+func (d *FakeDatabase) GetBuild(params *DatabaseGetBuildParams) (*DatabaseBuild, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	for _, b := range d.builds {
+		if b.DatabaseBuild.ID == params.ID {
+			if b.UserID != params.UserID {
+				return nil, errors.New("build access denied")
+			}
+			return &b.DatabaseBuild, nil
+		}
+	}
+	return nil, errors.New("build not found")
+}
+
+func (d *FakeDatabase) GetBuildCount(params *DatabaseGetBuildCountParams) (int, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	count := 0
+	for _, b := range d.builds {
+		if b.CreatedAt.Equal(params.StartTime) || b.CreatedAt.After(params.StartTime) && b.CreatedAt.Before(params.EndTime) {
+			if b.UserID == params.UserID {
+				count++
+			}
+		}
+	}
+	return count, nil
+}
+
+func (d *FakeDatabase) ListBuilds(params *DatabaseListBuildsParams) (*DatabaseListBuildsResult, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	builds := make([]*DatabaseBuild, 0)
+	pageOffset := 0
+	totalSize := 0
+	for _, b := range d.builds {
+		if b.UserID == params.UserID {
+			totalSize++
+			if pageOffset < params.PageOffset {
+				pageOffset++
+				continue
+			}
+			if len(builds) >= params.PageLimit {
+				continue
+			}
+			builds = append(builds, &b.DatabaseBuild)
+		}
+	}
+
+	var nextPageOffset *int = nil
+	if maybeNextPageOffset := pageOffset + len(builds); maybeNextPageOffset < totalSize {
+		nextPageOffset = new(int)
+		*nextPageOffset = maybeNextPageOffset
+	}
+
+	return &DatabaseListBuildsResult{
+		Builds:         builds,
+		NextPageOffset: nextPageOffset,
+		TotalSize:      totalSize,
+	}, nil
+}
+
+func (d *FakeDatabase) Begin() (tx Database, commit func() error, rollback func() error, err error) {
+	d.mu.Lock()
+	unlocked := false
+	unlock := func() {
+		if !unlocked {
+			d.mu.Unlock()
+			unlocked = true
+		}
+	}
+
+	fakeTx := &FakeDatabase{
+		// TODO: Future build-editing functions should replace builds,
+		// otherwise fake transaction wouldn't be rollbackable
+		// as it reuses FakeDatabaseBuilds via pointers.
+		builds: append([]*FakeDatabaseBuild(nil), d.builds...),
+	}
+	fakeCommit := func() error {
+		defer unlock()
+		d.builds = fakeTx.builds
+		return nil
+	}
+	fakeRollback := func() error {
+		defer unlock()
+		return nil
+	}
+	return fakeTx, fakeCommit, fakeRollback, nil
+}
+
+func (d *FakeDatabase) LockUser(params *DatabaseLockUserParams) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.muFromUserID == nil {
+		d.muFromUserID = make(map[uuid.UUID]*sync.Mutex)
+	}
+	if _, ok := d.muFromUserID[params.UserID]; !ok {
+		d.muFromUserID[params.UserID] = new(sync.Mutex)
+	}
+	d.muFromUserID[params.UserID].Lock()
+	return nil
 }
 
 // TODO: Add t.Parallel().
 func TestServiceCreateBuild(t *testing.T) {
+	config := &Config{
+		BuildsAllowed: 10,
+	}
+
+	fakeDatabase := &FakeDatabase{}
+
 	tests := []struct {
 		name              string
 		createBuildParams *CreateBuildParams
 		want              *Build
 		wantErr           error
-		config            *Config
-		mockDatabase      *MockDatabase
 	}{
 		{
 			"creates a build",
@@ -53,33 +183,12 @@ func TestServiceCreateBuild(t *testing.T) {
 				OutputFile:       nil,
 			},
 			nil,
-			&Config{
-				BuildsAllowed: 10,
-			},
-			&MockDatabase{
-				TransactWithUserFunc: func(userID uuid.UUID) (database Database, commit func() error, rollback func() error, err error) {
-					return &MockDatabase{
-						GetBuildCountFunc: func(userID uuid.UUID, startTime time.Time, endTime time.Time) (int, error) {
-							return 7, nil
-						},
-						CreateBuildFunc: func(contextToken string, documentFiles map[string][]byte, idempotencyKey uuid.UUID, userID uuid.UUID) (*Build, error) {
-							return &Build{
-								Done:             false,
-								Error:            nil,
-								ID:               uuid.MustParse("aaaaaaaa-0000-0000-0000-000000000000"),
-								NextContextToken: "",
-								OutputFile:       nil,
-							}, nil
-						},
-					}, func() error { return nil }, func() error { return nil }, nil
-				},
-			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			service := NewService(tt.config, tt.mockDatabase, 0, 0)
+			service := NewService(config, fakeDatabase, 0, 0)
 			got, gotErr := service.CreateBuild(tt.createBuildParams)
 			want, wantErr := tt.want, tt.wantErr
 			if !reflect.DeepEqual(got, want) || !errors.Is(gotErr, wantErr) {
