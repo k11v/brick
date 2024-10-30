09f9f740f2e8626293eccb2f322622df13f3c288
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-10-30 16:31:38 +0300

    Add PostgresDatabase

--- internal/build/database.go
+++ internal/build/database.go
@@ -1,1 +1,44 @@
 package build
+
+import "github.com/jackc/pgx/v5/pgxpool"
+
+type PostgresDatabase struct {
+	pool *pgxpool.Pool
+}
+
+var _ Database = (*PostgresDatabase)(nil)
+
+// BeginFunc implements Database.
+func (p *PostgresDatabase) BeginFunc(f func(tx Database) error) error {
+	panic("unimplemented")
+}
+
+// CreateBuild implements Database.
+func (p *PostgresDatabase) CreateBuild(params *DatabaseCreateBuildParams) (*DatabaseBuild, error) {
+	panic("unimplemented")
+}
+
+// GetBuild implements Database.
+func (p *PostgresDatabase) GetBuild(params *DatabaseGetBuildParams) (*DatabaseBuild, error) {
+	panic("unimplemented")
+}
+
+// GetBuildByIdempotencyKey implements Database.
+func (p *PostgresDatabase) GetBuildByIdempotencyKey(params *DatabaseGetBuildByIdempotencyKeyParams) (*DatabaseBuild, error) {
+	panic("unimplemented")
+}
+
+// GetBuildCount implements Database.
+func (p *PostgresDatabase) GetBuildCount(params *DatabaseGetBuildCountParams) (int, error) {
+	panic("unimplemented")
+}
+
+// ListBuilds implements Database.
+func (p *PostgresDatabase) ListBuilds(params *DatabaseListBuildsParams) (*DatabaseListBuildsResult, error) {
+	panic("unimplemented")
+}
+
+// LockUser implements Database.
+func (p *PostgresDatabase) LockUser(params *DatabaseLockUserParams) error {
+	panic("unimplemented")
+}
