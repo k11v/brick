16c55b3dd249391bb6d6fcb1c22bbe780034fa2b
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-10-31 00:38:34 +0300

    Add PostgresDatabase tests

--- internal/build/database_test.go
+++ internal/build/database_test.go
@@ -1,1 +1,33 @@
 package build
+
+import (
+	"testing"
+)
+
+func TestPostgresDatabaseBeginFunc(t *testing.T) {
+	t.SkipNow()
+}
+
+func TestPostgresDatabaseCreateBuild(t *testing.T) {
+	t.SkipNow()
+}
+
+func TestPostgresDatabaseGetBuild(t *testing.T) {
+	t.SkipNow()
+}
+
+func TestPostgresDatabaseGetBuildByIdempotencyKey(t *testing.T) {
+	t.SkipNow()
+}
+
+func TestPostgresDatabaseGetBuildCount(t *testing.T) {
+	t.SkipNow()
+}
+
+func TestPostgresDatabaseListBuilds(t *testing.T) {
+	t.SkipNow()
+}
+
+func TestPostgresDatabaseLockUser(t *testing.T) {
+	t.SkipNow()
+}
