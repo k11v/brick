9c2b15d678e622c92fe432613f21aff5da517e8f
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-10-31 02:54:52 +0300

    Add postgrestest.Setup that sets up a Postgres container

--- internal/build/database_test.go
+++ internal/build/database_test.go
@@ -1,7 +1,11 @@
 package build
 
 import (
+	"context"
 	"testing"
+
+	"github.com/k11v/brick/internal/postgrestest"
+	"github.com/k11v/brick/internal/postgresutil"
 )
 
 func TestPostgresDatabaseBeginFunc(t *testing.T) {
@@ -9,7 +13,24 @@ func TestPostgresDatabaseBeginFunc(t *testing.T) {
 }
 
 func TestPostgresDatabaseCreateBuild(t *testing.T) {
-	t.SkipNow()
+	ctx := context.Background()
+
+	connectionString, teardown, err := postgrestest.Setup(ctx)
+	t.Cleanup(func() {
+		if err := teardown(); err != nil {
+			t.Errorf("didn't want %v", err)
+		}
+	})
+
+	pool, err := postgresutil.NewPool(ctx, connectionString)
+	if err != nil {
+		t.Fatalf("didn't want %v", err)
+	}
+	if err = pool.Ping(ctx); err != nil {
+		t.Errorf("didn't want %v", err)
+	}
+
+	_ = pool
 }
 
 func TestPostgresDatabaseGetBuild(t *testing.T) {
