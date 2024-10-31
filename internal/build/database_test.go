3304e3264a1b5bd04bcfe058047e42f297c44968
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-10-31 03:09:50 +0300

    Fix postgrestest.Setup call to check error

--- internal/build/database_test.go
+++ internal/build/database_test.go
@@ -16,6 +16,9 @@ func TestPostgresDatabaseCreateBuild(t *testing.T) {
 	ctx := context.Background()
 
 	connectionString, teardown, err := postgrestest.Setup(ctx)
+	if err != nil {
+		t.Fatalf("didn't want %v", err)
+	}
 	t.Cleanup(func() {
 		if err := teardown(); err != nil {
 			t.Errorf("didn't want %v", err)
@@ -27,7 +30,7 @@ func TestPostgresDatabaseCreateBuild(t *testing.T) {
 		t.Fatalf("didn't want %v", err)
 	}
 	if err = pool.Ping(ctx); err != nil {
-		t.Errorf("didn't want %v", err)
+		t.Fatalf("didn't want %v", err)
 	}
 
 	_ = pool
