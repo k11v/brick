a3d07e92de2f49b4bb960c690696d17ab346effc
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-11-02 14:45:50 +0300

    Add builds table

--- internal/postgresprovision/migrations/0_init.up.sql
+++ internal/postgresprovision/migrations/0_init.up.sql
@@ -2,8 +2,16 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- -- TODO
+-- CREATE TABLE IF NOT EXISTS users ();
+
 CREATE TABLE IF NOT EXISTS builds (
-    id uuid DEFAULT uuid_generate_v4()
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    idempotency_key uuid NOT NULL,
+    status text NOT NULL,
+    user_id uuid NOT NULL,
+    PRIMARY KEY (id)
 );
 
 COMMIT;
