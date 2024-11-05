c6af086e6d9c677c375cd914dcb9aba3d070857e
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-11-05 13:00:25 +0300

    Add process and result columns to builds table

--- internal/postgresprovision/migrations/0_init.up.sql
+++ internal/postgresprovision/migrations/0_init.up.sql
@@ -7,11 +7,28 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS builds (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    created_at timestamp with time zone NOT NULL DEFAULT now(),
+
     idempotency_key uuid NOT NULL,
-    status text NOT NULL,
+
     user_id uuid NOT NULL,
-    PRIMARY KEY (id)
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+
+    document_files jsonb,
+    document_cache_files jsonb,
+
+    process_log_file text,
+    process_used_time interval,
+    process_used_memory integer,
+    process_exit_code integer,
+
+    result_pdf_file text,
+    result_cache_files jsonb,
+    result_expires_at timestamp with time zone,
+
+    status text NOT NULL,
+
+    PRIMARY KEY (id),
+    CHECK (status IN ('pending', 'running', 'completed', 'canceled'))
 );
 
 COMMIT;
