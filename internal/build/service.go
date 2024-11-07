4c9fe0251de1df231c2cf59e679b0d0e3b517a89
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-11-07 12:46:28 +0300

    Add new fields to internal struct Build

--- internal/build/service.go
+++ internal/build/service.go
@@ -14,13 +14,35 @@ var (
 	ErrDatabaseNotFound          = errors.New("database: not found")
 )
 
-// TODO: add Build.CreatedAt.
+type Status int
+
+const (
+	StatusCreated Status = iota
+	StatusRunning
+	StatusCompleted
+	StatusCanceled
+)
+
 type Build struct {
-	Done             bool
-	Error            error
-	ID               uuid.UUID
-	NextContextToken string
-	OutputFile       []byte
+	ID             uuid.UUID
+	IdempotencyKey uuid.UUID
+
+	UserID    uuid.UUID
+	CreatedAt time.Time
+
+	DocumentToken string // instead of DocumentCacheFiles map[string][]byte
+	DocumentFiles map[string][]byte
+
+	ProcessLogFile    []byte
+	ProcessUsedTime   time.Duration
+	ProcessUsedMemory int
+	ProcessExitCode   int
+
+	OutputFile        []byte
+	NextDocumentToken string // instead of OutputCacheFiles map[string][]byte
+	OutputExpiresAt   time.Time
+
+	Status Status
 }
 
 type Database interface {
--- internal/postgresprovision/migrations/0_init.up.sql
+++ internal/postgresprovision/migrations/0_init.up.sql
@@ -2,28 +2,24 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- -- TODO
--- CREATE TABLE IF NOT EXISTS users ();
-
 CREATE TABLE IF NOT EXISTS builds (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
-
     idempotency_key uuid NOT NULL,
 
     user_id uuid NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT now(),
 
+    document_token text, -- instead of document_cache_files jsonb
     document_files jsonb,
-    document_cache_files jsonb,
 
     process_log_file text,
     process_used_time interval,
     process_used_memory integer,
     process_exit_code integer,
 
-    result_pdf_file text,
-    result_cache_files jsonb,
-    result_expires_at timestamp with time zone,
+    output_file text,
+    next_document_token text, -- instead of output_cache_files jsonb
+    output_expires_at timestamp with time zone,
 
     status text NOT NULL,
 
