a064df1f2c41e0c17b12aa5f85c631073a0ebbc2
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-11-12 21:04:28 +0300

    Move Querier

---
+++ internal/app/build/oper/operpostgres/querier.go
@@ -0,0 +1,22 @@
+package operpostgres
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Querier interface {
+	Begin(ctx context.Context) (pgx.Tx, error)
+	Exec(ctx context.Context, sql string, arguments ...any) (commandTag pgconn.CommandTag, err error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
+}
+
+var (
+	_ Querier = (*pgxpool.Pool)(nil)
+	_ Querier = pgx.Tx(nil)
+)
--- internal/build/database.go
+++ internal/build/database.go
@@ -7,24 +7,9 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
-	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var _ Database = (*PostgresDatabase)(nil)
-
-var (
-	_ Querier = (*pgxpool.Pool)(nil)
-	_ Querier = pgx.Tx(nil)
-)
-
-type Querier interface {
-	Begin(ctx context.Context) (pgx.Tx, error)
-	Exec(ctx context.Context, sql string, arguments ...any) (commandTag pgconn.CommandTag, err error)
-	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
-	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
-	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
-}
 
 type PostgresDatabase struct {
 	db Querier // required
