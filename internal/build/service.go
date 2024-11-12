a2b553c2caf6366cc2572435c6e4faba939301a4
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-11-12 20:46:19 +0300

    Move Build

--- internal/build/service.go
+++ internal/build/service.go
@@ -14,37 +14,6 @@ var (
 	ErrDatabaseNotFound          = errors.New("database: not found")
 )
 
-type Status int
-
-const (
-	StatusCreated Status = iota
-	StatusRunning
-	StatusCompleted
-	StatusCanceled
-)
-
-type Build struct {
-	ID             uuid.UUID
-	IdempotencyKey uuid.UUID
-
-	UserID    uuid.UUID
-	CreatedAt time.Time
-
-	DocumentToken string // instead of DocumentCacheFiles map[string][]byte
-	DocumentFiles map[string][]byte
-
-	ProcessLogFile    []byte
-	ProcessUsedTime   time.Duration
-	ProcessUsedMemory int
-	ProcessExitCode   int
-
-	OutputFile        []byte
-	NextDocumentToken string // instead of OutputCacheFiles map[string][]byte
-	OutputExpiresAt   time.Time
-
-	Status Status
-}
-
 type Database interface {
 	BeginFunc(ctx context.Context, f func(tx Database) error) error
 	LockUser(ctx context.Context, params *DatabaseLockUserParams) error
