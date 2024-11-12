dfd78e1d3fea378ab074c3016589e3d0310832dd
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-11-13 00:40:37 +0300

    Add Database.CreateBuild impl

--- internal/postgresprovision/migrations/0_init.up.sql
+++ internal/postgresprovision/migrations/0_init.up.sql
@@ -9,15 +9,18 @@ CREATE TABLE IF NOT EXISTS builds (
     user_id uuid NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT now(),
 
-    document_token text, -- instead of document_cache_files jsonb
-    document_files jsonb,
+    -- document_token text, -- instead of document_cache_files jsonb
+    -- document_files jsonb,
+    document_token text,
 
-    process_log_file text,
+    -- process_log_file text,
+    process_log_token text,
     process_used_time interval,
     process_used_memory integer,
     process_exit_code integer,
 
-    output_file text,
+    -- output_file text,
+    output_token text,
     next_document_token text, -- instead of output_cache_files jsonb
     output_expires_at timestamp with time zone,
 
