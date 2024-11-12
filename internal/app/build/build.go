6d1e4bfec82a413e2abeede35c0b43af3cd3ddcf
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-11-12 20:46:19 +0300

    Move Build

--- internal/app/build/build.go
+++ internal/app/build/build.go
@@ -1,1 +1,38 @@
 package build
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Status int
+
+const (
+	StatusCreated Status = iota
+	StatusRunning
+	StatusCompleted
+	StatusCanceled
+)
+
+type Build struct {
+	ID             uuid.UUID
+	IdempotencyKey uuid.UUID
+
+	UserID    uuid.UUID
+	CreatedAt time.Time
+
+	DocumentToken string // instead of DocumentCacheFiles map[string][]byte
+	DocumentFiles map[string][]byte
+
+	ProcessLogFile    []byte
+	ProcessUsedTime   time.Duration
+	ProcessUsedMemory int
+	ProcessExitCode   int
+
+	OutputFile        []byte
+	NextDocumentToken string // instead of OutputCacheFiles map[string][]byte
+	OutputExpiresAt   time.Time
+
+	Status Status
+}
