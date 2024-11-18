18601e611a97c75f4fc6e29c19b2a7c5ffe3d778
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-11-18 13:23:57 +0300

    Add UploadFiles and DownloadFiles to Storage interface

--- internal/app/build/operation/storage.go
+++ internal/app/build/operation/storage.go
@@ -1,3 +1,23 @@
 package operation
 
-type Storage interface{}
+import (
+	"context"
+	"mime/multipart"
+
+	"github.com/google/uuid"
+)
+
+type StorageUploadFilesParams struct {
+	BuildID         uuid.UUID
+	MultipartReader *multipart.Reader
+}
+
+type StorageDownloadFilesParams struct {
+	BuildID         uuid.UUID
+	MultipartWriter *multipart.Writer
+}
+
+type Storage interface {
+	UploadFiles(ctx context.Context, params *StorageUploadFilesParams) error
+	DownloadFiles(ctx context.Context, params *StorageDownloadFilesParams) error
+}
