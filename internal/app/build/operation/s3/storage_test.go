cb786c87a6cd05d51e489ebffaa409335ccd91ab
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-11-18 13:29:58 +0300

    Add TestStorage without impl

---
+++ internal/app/build/operation/s3/storage_test.go
@@ -0,0 +1,12 @@
+package s3
+
+import "testing"
+
+func TestStorage(t *testing.T) {
+	t.Run("uploads and downloads files", func(t *testing.T) {
+	})
+}
+
+func NewTestStorage() *Storage {
+	panic("unimplemented")
+}
