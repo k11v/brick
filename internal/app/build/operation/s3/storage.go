166e876e85fcdca073400c5f7a10cb3763cd13f6
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-11-18 14:51:27 +0300

    Add NewStorage impl

--- internal/app/build/operation/s3/storage.go
+++ internal/app/build/operation/s3/storage.go
@@ -2,16 +2,42 @@ package s3
 
 import (
 	"context"
+	"net/url"
+
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	transport "github.com/aws/smithy-go/endpoints"
 
 	"github.com/k11v/brick/internal/app/build/operation"
 )
 
 var _ operation.Storage = (*Storage)(nil)
 
-type Storage struct{}
+type Storage struct {
+	client *s3.Client
+}
 
+// NewStorage creates a new Storage instance using the provided connection string.
+// The connection string must be a valid URL in the format: http://key:secret@s3:9000.
+// For MinIO, the key and secret are the username and password respectively.
+// It panics if the connection string is not a valid URL.
 func NewStorage(connectionString string) *Storage {
-	panic("unimplemented")
+	u, err := url.Parse(connectionString)
+	if err != nil {
+		panic(err)
+	}
+
+	username := u.User.Username()
+	password, _ := u.User.Password()
+	u.User = nil
+
+	client := s3.New(
+		s3.Options{
+			Credentials:        credentials.NewStaticCredentialsProvider(username, password, ""),
+			EndpointResolverV2: &endpointResolver{BaseURL: u},
+		},
+	)
+	return &Storage{client: client}
 }
 
 // UploadFiles implements operation.Storage.
@@ -22,4 +48,16 @@ func (s *Storage) UploadFiles(ctx context.Context, params *operation.StorageUplo
 // DownloadFiles implements operation.Storage.
 func (s *Storage) DownloadFiles(ctx context.Context, params *operation.StorageDownloadFilesParams) error {
 	panic("unimplemented")
+}
+
+// endpointResolver implements s3.EndpointResolverV2.
+// It resolves endpoints for S3-compatible object storage like MinIO.
+type endpointResolver struct {
+	BaseURL *url.URL // required
+}
+
+func (r *endpointResolver) ResolveEndpoint(_ context.Context, params s3.EndpointParameters) (transport.Endpoint, error) {
+	u := *r.BaseURL
+	u.Path += "/" + *params.Bucket
+	return transport.Endpoint{URI: u}, nil
 }
