7b75b73510bff27752b8d7336b6bd80d44dbac01
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-11-18 13:40:54 +0300

    Add NewTestStorage impl

--- internal/app/build/operation/s3/storage_test.go
+++ internal/app/build/operation/s3/storage_test.go
@@ -1,12 +1,57 @@
 package s3
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
 
 func TestStorage(t *testing.T) {
 	t.Run("uploads and downloads files", func(t *testing.T) {
+		ctx := context.Background()
+		storage := NewTestStorage(t, ctx)
+		_ = storage
 	})
 }
 
-func NewTestStorage() *Storage {
-	panic("unimplemented")
+func NewTestStorage(tb testing.TB, ctx context.Context) *Storage {
+	tb.Helper()
+
+	username := "minioadmin"
+	password := "minioadmin"
+
+	req := testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:        "quay.io/minio/minio:latest",
+			ExposedPorts: []string{"9000/tcp"},
+			WaitingFor:   wait.ForHTTP("/minio/health/live").WithPort("9000"),
+			Env: map[string]string{
+				"MINIO_ROOT_USER":     username,
+				"MINIO_ROOT_PASSWORD": password,
+			},
+			Cmd: []string{"server", "/data"},
+		},
+		Started: true,
+	}
+
+	c, err := testcontainers.GenericContainer(ctx, req)
+	testcontainers.CleanupContainer(tb, c)
+	if err != nil {
+		tb.Fatalf("didn't want %q", err)
+	}
+
+	host, err := c.Host(ctx)
+	if err != nil {
+		tb.Fatalf("didn't want %q", err)
+	}
+	port, err := c.MappedPort(ctx, "9000/tcp")
+	if err != nil {
+		tb.Fatalf("didn't want %q", err)
+	}
+	connectionString := fmt.Sprintf("%s:%s", host, port.Port())
+
+	return NewStorage(connectionString)
 }
