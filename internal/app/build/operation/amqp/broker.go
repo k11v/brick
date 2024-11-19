e2892b3f9090584e7bab33fead09cfc2364dc960
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-11-19 14:47:21 +0300

    Add ReceiveBuildTask impl draft

--- internal/app/build/operation/amqp/broker.go
+++ internal/app/build/operation/amqp/broker.go
@@ -1,6 +1,7 @@
 package amqp
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -45,13 +46,14 @@ func (broker Broker) SendBuildTask(ctx context.Context, b *build.Build) error {
 		return fmt.Errorf("send build task: %w", err)
 	}
 
-	body, err := json.Marshal(b)
-	if err != nil {
+	contentType := "application/json"
+	body := &bytes.Buffer{}
+	if err := json.NewEncoder(body).Encode(b); err != nil {
 		return fmt.Errorf("send build task: %w", err)
 	}
 	msg := amqp091.Publishing{
-		ContentType: "application/json",
-		Body:        body,
+		ContentType: contentType,
+		Body:        body.Bytes(),
 	}
 
 	err = ch.PublishWithContext(ctx,
@@ -68,6 +70,61 @@ func (broker Broker) SendBuildTask(ctx context.Context, b *build.Build) error {
 	return nil
 }
 
-func (Broker) ReceiveBuildTask() (*build.Build, error) {
-	panic("unimplemented")
+// TODO: Consider returning a channel.
+func (broker Broker) ReceiveBuildTask(ctx context.Context) (*build.Build, error) {
+	conn, err := amqp091.Dial(broker.connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("receive build task: %w", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("receive build task: %w", err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err != nil {
+		return nil, fmt.Errorf("receive build task: %w", err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		return nil, fmt.Errorf("receive build task: %w", err)
+	}
+
+	var msg amqp091.Delivery
+	select {
+	case msg = <-msgs:
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+
+	var b build.Build
+	dec := json.NewDecoder(bytes.NewReader(msg.Body))
+	dec.DisallowUnknownFields()
+	if err = dec.Decode(&b); err != nil {
+		return nil, fmt.Errorf("receive build task: %w", err)
+	}
+	if dec.More() {
+		return nil, fmt.Errorf("receive build task: multiple top-level values")
+	}
+
+	return &b, nil
 }
