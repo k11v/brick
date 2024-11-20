f563be2c06a7ac65dc80438150b28932603eedf7
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-11-20 17:06:33 +0300

    Add methods to Broker interface

--- internal/app/build/operation/broker.go
+++ internal/app/build/operation/broker.go
@@ -1,3 +1,12 @@
 package operation
 
-type Broker interface{}
+import (
+	"context"
+
+	"github.com/k11v/brick/internal/app/build"
+)
+
+type Broker interface {
+	SendBuildTask(ctx context.Context, b *build.Build) error
+	ReceiveBuildTask(ctx context.Context) (*build.Build, error)
+}
