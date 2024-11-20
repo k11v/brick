e968fc50b4638d8d02533e47371acdfa52201781
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-11-20 17:06:33 +0300

    Add methods to Broker interface

--- internal/app/build/operation/broker_stub_test.go
+++ internal/app/build/operation/broker_stub_test.go
@@ -1,5 +1,19 @@
 package operation
 
+import (
+	"context"
+
+	"github.com/k11v/brick/internal/app/build"
+)
+
 var _ Broker = (*StubBroker)(nil)
 
 type StubBroker struct{}
+
+func (StubBroker) SendBuildTask(ctx context.Context, b *build.Build) error {
+	return nil
+}
+
+func (StubBroker) ReceiveBuildTask(ctx context.Context) (*build.Build, error) {
+	return &build.Build{}, nil
+}
