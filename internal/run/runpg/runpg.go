36b692f183467383792622726359341186584cbd
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-11-24 00:04:26 +0300

    Extract pgutil into runpg

--- internal/run/runpg/runpg.go
+++ internal/run/runpg/runpg.go
@@ -1,1 +1,25 @@
 package runpg
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Config struct {
+	ConnectionString string // required
+}
+
+func NewPool(ctx context.Context, conf Config) (*pgxpool.Pool, error) {
+	pgxConf, err := pgxpool.ParseConfig(conf.ConnectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, pgxConf)
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
