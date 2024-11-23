90334eb25e13d89989b2578593bcc63d657c7685
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-11-24 00:47:35 +0300

    Tidy

--- internal/run/runs3/runs3.go
+++ internal/run/runs3/runs3.go
@@ -2,37 +2,12 @@ package runs3
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"net/url"
-	"time"
 
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	transport "github.com/aws/smithy-go/endpoints"
 )
-
-// BucketName is the name of the S3 bucket used by the application.
-//
-// It is a hard-coded string because the application uses MinIO
-// instead of Amazon S3, and MinIO isn't expected to be shared.
-//
-// It is a also variable instead of a constant because aws-sdk-go-v2 often
-// requires a pointer to a string which is easier to acqurie with a variable.
-var BucketName = "brick"
-
-// endpointResolver implements s3.EndpointResolverV2.
-// It resolves endpoints for S3-compatible object storage like MinIO.
-type endpointResolver struct {
-	BaseURL *url.URL // required
-}
-
-func (r *endpointResolver) ResolveEndpoint(_ context.Context, params s3.EndpointParameters) (transport.Endpoint, error) {
-	u := *r.BaseURL
-	u.Path += "/" + *params.Bucket
-	return transport.Endpoint{URI: u}, nil
-}
 
 // NewClient creates a new Client using the provided connection string.
 // The connection string must be a valid URL in the format: http://key:secret@s3:9000.
@@ -57,28 +32,14 @@ func NewClient(connectionString string) *s3.Client {
 	return client
 }
 
-// Setup shouldn't be used with AWS as is because it doesn't specify the region.
-// See NewClient for connection string format and panic conditions.
-func Setup(ctx context.Context, connectionString string) error {
-	client := NewClient(connectionString)
+// endpointResolver implements s3.EndpointResolverV2.
+// It resolves endpoints for S3-compatible object storage like MinIO.
+type endpointResolver struct {
+	BaseURL *url.URL // required
+}
 
-	_, err := client.CreateBucket(ctx, &s3.CreateBucketInput{
-		Bucket: &BucketName,
-	})
-	if ownedErr := (*types.BucketAlreadyOwnedByYou)(nil); errors.As(err, &ownedErr) {
-		// continue
-	} else if err != nil {
-		return fmt.Errorf("setup: %w", err)
-	}
-
-	err = s3.NewBucketExistsWaiter(client).Wait(
-		ctx,
-		&s3.HeadBucketInput{Bucket: &BucketName},
-		time.Minute,
-	)
-	if err != nil {
-		return fmt.Errorf("setup: %w", err)
-	}
-
-	return nil
+func (r *endpointResolver) ResolveEndpoint(_ context.Context, params s3.EndpointParameters) (transport.Endpoint, error) {
+	u := *r.BaseURL
+	u.Path += "/" + *params.Bucket
+	return transport.Endpoint{URI: u}, nil
 }
---
+++ internal/run/runs3/setup.go
@@ -0,0 +1,46 @@
+package runs3
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+)
+
+// BucketName is the name of the S3 bucket used by the application.
+//
+// It is a hard-coded string because the application uses MinIO
+// instead of Amazon S3, and MinIO isn't expected to be shared.
+//
+// It is a also variable instead of a constant because aws-sdk-go-v2 often
+// requires a pointer to a string which is easier to acqurie with a variable.
+var BucketName = "brick"
+
+// Setup shouldn't be used with AWS as is because it doesn't specify the region.
+// See NewClient for connection string format and panic conditions.
+func Setup(ctx context.Context, connectionString string) error {
+	client := NewClient(connectionString)
+
+	_, err := client.CreateBucket(ctx, &s3.CreateBucketInput{
+		Bucket: &BucketName,
+	})
+	if ownedErr := (*types.BucketAlreadyOwnedByYou)(nil); errors.As(err, &ownedErr) {
+		// continue
+	} else if err != nil {
+		return fmt.Errorf("setup: %w", err)
+	}
+
+	err = s3.NewBucketExistsWaiter(client).Wait(
+		ctx,
+		&s3.HeadBucketInput{Bucket: &BucketName},
+		time.Minute,
+	)
+	if err != nil {
+		return fmt.Errorf("setup: %w", err)
+	}
+
+	return nil
+}
