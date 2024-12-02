346add695169d2bbe9ec84fb4b2a3832f7fe9f57
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-12-02 03:24:33 +0300

    Add RunWrapper part with output processing

--- internal/build/wrapper_test.go
+++ internal/build/wrapper_test.go
@@ -7,14 +7,14 @@ import (
 
 func TestWrapper(t *testing.T) {
 	t.Run("runs", func(t *testing.T) {
-		in := bytes.NewReader([]byte("Content-Type: multipart/mixed; boundary=foo\r\n" +
+		in := bytes.NewReader([]byte("Content-Type: multipart/form-data; boundary=foo\r\n" +
 			"\r\n" +
 			"--foo\r\n" +
 			"Content-Type: application/json\r\n" +
 			"\r\n" +
 			`{"foo": "string", "bar": 42}` + "\r\n" +
 			"--foo--\r\n"))
-		out := new(bytes.Buffer)
+		out := new(bytes.Buffer) // e.g. "Content-Type: multipart/form-data; boundary=1751ed1b0d005fc0b53c86b5330ddc0fa4cb63cbf73a61c47df95aafb962\r\n\r\n--1751ed1b0d005fc0b53c86b5330ddc0fa4cb63cbf73a61c47df95aafb962\r\nContent-Type: applcation/json\r\n\r\n{\"Baz\":\"string\"}\n\r\n--1751ed1b0d005fc0b53c86b5330ddc0fa4cb63cbf73a61c47df95aafb962\r\nContent-Type: application/octet-stream\r\n\r\nfile content\r\n--1751ed1b0d005fc0b53c86b5330ddc0fa4cb63cbf73a61c47df95aafb962\r\nContent-Type: application/octet-stream\r\n\r\nfile content\r\n--1751ed1b0d005fc0b53c86b5330ddc0fa4cb63cbf73a61c47df95aafb962\r\nContent-Type: application/octet-stream\r\n\r\nfile content\r\n--1751ed1b0d005fc0b53c86b5330ddc0fa4cb63cbf73a61c47df95aafb962--\r\n"
 
 		if err := RunWrapper(in, out); err != nil {
 			t.Fatalf("didn't want %q", err)
