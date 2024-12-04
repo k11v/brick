8f3ae8f921047192841dee782be2f1857999e5b0
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-12-04 17:14:16 +0300

    Add runSend to worker for development

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+	// TODO: Remove.
+	var send bool
+	flag.BoolVar(&send, "send", false, "send a message to the worker")
+	flag.Parse()
+
+	// TODO: Remove.
+	if send {
+		if err := runSend(); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
 	if err := run(); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
@@ -14,5 +29,10 @@ func main() {
 }
 
 func run() error {
+	return nil
+}
+
+// TODO: Remove.
+func runSend() error {
 	return nil
 }
