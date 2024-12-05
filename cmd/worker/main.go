2e129aa7091ed3dbbf89891a6b22b7e10c18db06
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-12-05 19:50:43 +0300

    Add runSend impl

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"log/slog"
 	"os"
+
+	"github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -34,5 +40,34 @@ func run() error {
 
 // TODO: Remove.
 func runSend() error {
+	ctx := context.Background()
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare("example", false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	msg := amqp091.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("Hello World!"),
+	}
+	err = ch.PublishWithContext(ctx, "", q.Name, false, false, msg)
+	if err != nil {
+		return err
+	}
+	slog.Default().Info("sent", "msg", string(msg.Body))
+
 	return nil
 }
