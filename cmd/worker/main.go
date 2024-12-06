02e6cef7e4149253a9ba748526b621e61ebed21a
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-12-06 13:21:24 +0300

    Add run impl with consumer retries

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/rabbitmq/amqp091-go"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -35,8 +36,13 @@ func main() {
 }
 
 func run() error {
+	ctx := context.Background()
+
+	maxRetries := 15
+	retries := 0
+
 	for {
-		funcErr := func() error {
+		consumeErr := func() error {
 			conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 			if err != nil {
 				return err
@@ -65,14 +71,47 @@ func run() error {
 
 			for msg := range msgs {
 				slog.Default().Info("received", "msg", string(msg.Body))
-				if err = msg.Ack(false); err != nil {
-					slog.Default().Error("didn't acknowledge", "err", err)
+
+				// TODO:
+				// Good handler must call msg.Ack, msg.Nack, or msg.Reject.
+				// These methods can fail and when they do, the channel
+				// becomes invalid which LIKELY means it closes.
+				// In this case We MAYBE need to range over the msgs
+				// until the end and drop every msg we encounter
+				// to prevent memory leaks or deadlocks.
+				// The handler should not be called when we do this.
+				// Right now we also rely on the channel not being closed
+				// after message acknowledgement to reset retries.
+				// Better API would MAYBE automatically issue a msg.Reject
+				// when the handler forgets to acknowledge do anything.
+				func(msg amqp.Delivery) {
+					if err = msg.Ack(false); err != nil {
+						slog.Default().Error("didn't acknowledge", "err", err)
+					}
+				}(msg)
+
+				if retries > 0 && !ch.IsClosed() {
+					slog.Default().Info("recovered", "retries", retries)
+					retries = 0
 				}
 			}
 
 			return errors.New("delivery channel is closed")
 		}()
-		slog.Default().Info("retrying", "err", funcErr)
+		slog.Default().Error("didn't consume", "err", consumeErr)
+
+		if retries > maxRetries {
+			return errors.New("max retries is exceeded")
+		}
+		retries++
+
+		select {
+		case <-time.After(time.Second):
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+
+		slog.Default().Info("retrying")
 	}
 }
 
