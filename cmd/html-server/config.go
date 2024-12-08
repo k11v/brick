6234a4b149d23793db24a607234cca1fccdeafec
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-12-09 00:16:41 +0300

    Add html-server decl with HTTP server

---
+++ cmd/html-server/config.go
@@ -0,0 +1,26 @@
+package main
+
+import "time"
+
+type config struct {
+	Host              string // default: "127.0.0.1"
+	Port              int    // default: 8080
+	ReadHeaderTimeout time.Duration
+	Development       bool
+}
+
+func (cfg *config) host() string {
+	h := cfg.Host
+	if h == "" {
+		h = "127.0.0.1"
+	}
+	return h
+}
+
+func (cfg *config) port() int {
+	p := cfg.Port
+	if p == 0 {
+		p = 8080
+	}
+	return p
+}
