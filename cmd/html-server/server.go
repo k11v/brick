7f13537b67b70c41a4fc940ddff6c53f9dc4d5d8
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-12-09 18:56:58 +0300

    Add server part with main, 404 and 500 pages

--- cmd/html-server/server.go
+++ cmd/html-server/server.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"log/slog"
 	"net"
 	"net/http"
 	"strconv"
+
+	"github.com/k11v/brick/cmd/html-server/static"
+)
+
+var (
+	funcs = template.FuncMap{}
+	fs    = static.FS
 )
 
 func newServer(conf *config) *http.Server {
@@ -14,9 +23,21 @@ func newServer(conf *config) *http.Server {
 	subLogLogger := slog.NewLogLogger(subLogger.Handler(), slog.LevelError)
 
 	mux := &http.ServeMux{}
-	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("ok"))
+		err := writePage(w, nil, "main.tmpl")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_ = writeErrorPage(w, http.StatusInternalServerError)
+		}
+	})
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		err := writeErrorPage(w, http.StatusNotFound)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_ = writeErrorPage(w, http.StatusInternalServerError)
+		}
 	})
 
 	return &http.Server{
@@ -25,4 +46,18 @@ func newServer(conf *config) *http.Server {
 		Handler:           mux,
 		ReadHeaderTimeout: conf.ReadHeaderTimeout,
 	}
+}
+
+// writePage.
+// The first template name is the one being executed.
+func writePage(w io.Writer, data any, templateNames ...string) error {
+	tmpl := template.Must(template.New(templateNames[0]).Funcs(funcs).ParseFS(fs, templateNames...))
+	if err := tmpl.Execute(w, data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeErrorPage(w io.Writer, statusCode int) error {
+	return writePage(w, statusCode, "error.tmpl")
 }
