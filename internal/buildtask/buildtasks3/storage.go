c2c7d955945005e4072a544cac39985bfe266911
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-12-15 23:56:33 +0300

    Rename buildtasks3 files

--- internal/buildtask/buildtasks3/storage.go
+++ internal/buildtask/buildtasks3/storage.go
@@ -5,91 +5,122 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"iter"
-	"mime/multipart"
 	"path"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/smithy-go"
 
-	"github.com/k11v/brick/internal/build"
 	"github.com/k11v/brick/internal/buildtask"
 	"github.com/k11v/brick/internal/run/runs3"
 )
 
-var _ buildtask.Storage = (*Storage)(nil)
-
-type Storage struct {
-	client *s3.Client
-
-	// uploadPartSize should be greater than or equal 5MB.
-	// See github.com/aws/aws-sdk-go-v2/feature/s3/manager.
-	uploadPartSize int
-
-	// downloadPartSize should be greater than or equal 5MB.
-	// See github.com/aws/aws-sdk-go-v2/feature/s3/manager.
-	downloadPartSize int
-}
-
-// NewStorage creates a new Storage using the provided connection string.
-// It panics if the connection string is not a valid URL.
-func NewStorage(connectionString string) *Storage {
-	return &Storage{
-		client:           runs3.NewClient(connectionString),
-		uploadPartSize:   10 * 1024 * 1024, // 10MB
-		downloadPartSize: 10 * 1024 * 1024, // 10MB
-	}
-}
-
-func (s *Storage) UploadFileV2(ctx context.Context, key string, r io.Reader) error {
-	return nil
-}
-
-type StorageDownloadFileV2Params struct{}
-
-type StorageDownloadFileV2Result struct{}
-
-func (s *Storage) DownloadFileV2(ctx context.Context, params *StorageDownloadFileV2Params) (*StorageDownloadFileV2Result, error) {
-	return nil, nil
-}
-
-func (s *Storage) UploadDirV2(ctx context.Context, prefix string, files iter.Seq2[*build.File, error]) error {
+// UploadFiles implements buildtask.Storage.
+// FIXME: p.FileName() returns only the last component and is platform-dependent when we want the full path.
+// TODO: consider the error related to manager.MaxUploadParts when handling uploader.Upload.
+// FIXME: When UploadFiles fails, it should clean up the files it has possibly already uploaded.
+func (s *Storage) UploadFiles(ctx context.Context, params *buildtask.StorageUploadFilesParams) error {
 	uploader := manager.NewUploader(s.client, func(u *manager.Uploader) {
 		u.PartSize = int64(s.uploadPartSize)
 	})
 
-	for file, err := range files {
-		if err != nil {
-			return fmt.Errorf("buildtasks3.Storage: %w", err)
+	for {
+		p, err := params.MultipartReader.NextPart()
+		if errors.Is(err, io.EOF) {
+			break
+		} else if err != nil {
+			return fmt.Errorf("storage upload files: %w", err)
 		}
 
-		key := path.Join(prefix, file.Name)
+		objectKey := path.Join(params.BuildID.String(), p.FileName())
+
 		_, err = uploader.Upload(ctx, &s3.PutObjectInput{
 			Bucket: &runs3.BucketName,
-			Key:    &key,
-			Body:   file.Content,
+			Key:    &objectKey,
+			Body:   p,
 		})
-		if err != nil {
-			if apiErr := smithy.APIError(nil); errors.As(err, &apiErr) && apiErr.ErrorCode() == "EntityTooLarge" {
-				err = errors.Join(buildtask.FileTooLarge, err)
-			}
-			return fmt.Errorf("buildtasks3.Storage: %w", err)
+		var apiErr smithy.APIError
+		if errors.As(err, &apiErr) && apiErr.ErrorCode() == "EntityTooLarge" {
+			return buildtask.FileTooLarge
+		} else if err != nil {
+			return fmt.Errorf("storage upload files: %w", err)
 		}
 
-		err = s3.NewObjectExistsWaiter(s.client).Wait(ctx, &s3.HeadObjectInput{
-			Bucket: &runs3.BucketName,
-			Key:    &key,
-		}, time.Minute)
+		err = s3.NewObjectExistsWaiter(s.client).Wait(
+			ctx,
+			&s3.HeadObjectInput{
+				Bucket: &runs3.BucketName,
+				Key:    &objectKey,
+			},
+			time.Minute,
+		)
 		if err != nil {
-			return fmt.Errorf("buildtasks3.Storage: %w", err)
+			return fmt.Errorf("storage upload files: %w", err)
 		}
 	}
 
 	return nil
 }
 
-func (s *Storage) DownloadDirV2(ctx context.Context, prefix string) (*multipart.Reader, error) {
-	return nil, nil
+// DownloadFiles implements buildtask.Storage.
+func (s *Storage) DownloadFiles(ctx context.Context, params *buildtask.StorageDownloadFilesParams) error {
+	downloader := manager.NewDownloader(s.client, func(d *manager.Downloader) {
+		d.PartSize = int64(s.downloadPartSize)
+		d.Concurrency = 1
+	})
+
+	objectPrefix := params.BuildID.String() + "/"
+
+	paginator := s3.NewListObjectsV2Paginator(s.client, &s3.ListObjectsV2Input{
+		Bucket: &runs3.BucketName,
+		Prefix: &objectPrefix,
+	})
+
+	fileNumber := 0
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return fmt.Errorf("storage download files: %w", err)
+		}
+
+		for _, object := range page.Contents {
+			fileNumber++
+
+			fileName, found := strings.CutPrefix(*object.Key, objectPrefix)
+			if !found {
+				panic("want prefix")
+			}
+
+			p, err := params.MultipartWriter.CreateFormFile(strconv.Itoa(fileNumber), fileName) // TODO: consider sending and receiving files in a tar archive
+			if err != nil {
+				return fmt.Errorf("storage download files: %w", err)
+			}
+
+			// fakeWriterAt needs manager.Downloader.Concurrency set to 1.
+			_, err = downloader.Download(ctx, fakeWriterAt{p}, &s3.GetObjectInput{
+				Bucket: &runs3.BucketName,
+				Key:    object.Key,
+			})
+			if err != nil {
+				return fmt.Errorf("storage download files: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+// fakeWriterAt wraps an io.Writer to provide a fake WriteAt method.
+// This method simply calls w.Write ignoring the offset parameter.
+// It can be used with github.com/aws/aws-sdk-go-v2/feature/s3/manager.Downloader.Download
+// if its concurrency is set to 1 because this guarantees the sequential writes.
+type fakeWriterAt struct {
+	w io.Writer // required
+}
+
+func (writerAt fakeWriterAt) WriteAt(p []byte, _ int64) (n int, err error) {
+	return writerAt.w.Write(p)
 }
