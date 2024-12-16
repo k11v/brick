aef03c70d32334101ccef817fc98c907c1d7fb3c
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-12-16 21:50:29 +0300

    Remove type column from operations table

--- internal/run/runpg/migrations/0_init.up.sql
+++ internal/run/runpg/migrations/0_init.up.sql
@@ -7,24 +7,20 @@ CREATE TABLE IF NOT EXISTS operations (
     idempotency_key uuid NOT NULL,
     user_id uuid NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT now(),
-    output_pdf_file_id uuid NOT NULL, -- TODO: NULL?
-    process_log_file_id uuid NOT NULL, -- TODO: NULL?
-    process_exit_code integer,
-    PRIMARY KEY (id),
-    FOREIGN KEY (output_pdf_file_id) REFERENCES operation_files (id),
-    FOREIGN KEY (process_log_file_id) REFERENCES operation_files (id)
+    output_file_key text NOT NULL,
+    log_file_key text NOT NULL,
+    exit_code integer,
+    PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX operations_idempotency_key_idx ON operations (idempotency_key);
 
-CREATE TABLE IF NOT EXISTS operation_files (
+CREATE TABLE IF NOT EXISTS operation_input_files (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
     operation_id uuid NOT NULL,
-    type text NOT NULL,
     name text NOT NULL,
     content_key text NOT NULL, -- TODO: NULL?
     PRIMARY KEY (id),
-    FOREIGN KEY (operation_id) REFERENCES operations (id),
-    CHECK (type IN ('input', 'output', 'run'))
+    FOREIGN KEY (operation_id) REFERENCES operations (id)
 );
 
 CREATE TABLE IF NOT EXISTS user_locks (
