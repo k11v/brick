7a611c610bdc22a2a7ae7a7ec3854707f362ed15
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-12-18 16:58:44 +0300

    Add OperationRunner.Run part with inner Run call

--- internal/build/runner.go
+++ internal/build/runner.go
@@ -1,9 +1,29 @@
 package build
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
 type OperationRunner struct{}
 
-type OperationRunnerRunParams struct{}
+type OperationRunnerRunParams struct {
+	ID     uuid.UUID
+	UserID uuid.UUID
+}
 
-func (r *OperationRunner) Run(params *OperationRunnerRunParams) error {
-	return nil
+type OperationRunnerRunResult struct{}
+
+func (r *OperationRunner) Run(params *OperationRunnerRunParams) (*OperationRunnerRunResult, error) {
+	runResult, err := Run(&RunParams{
+		InputDir:  "",
+		OutputDir: "",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("build.OperationRunner: %w", err)
+	}
+	_ = runResult
+
+	return &OperationRunnerRunResult{}, nil
 }
