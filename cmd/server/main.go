469d0273533cee94464f04a1e383c9c9ea10f310
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-12-19 17:32:46 +0300

    Change server main to not use run

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,20 +9,13 @@ import (
 )
 
 func main() {
-	if err := run(); err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
-	os.Exit(0)
-}
-
-func run() error {
 	server := newServer(&config{})
 
 	slog.Info("starting server", "addr", server.Addr)
 	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		return err
+		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
 	}
 
-	return nil
+	os.Exit(0)
 }
