e30d33a661ec9b65765631ef86714cc6d6be099b
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-12-19 17:33:50 +0300

    Remove worker runSend

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -3,32 +3,16 @@ package main
 import (
 	"context"
 	"errors"
-	"flag"
 	"fmt"
 	"log/slog"
 	"math/rand/v2"
 	"os"
 	"time"
 
-	"github.com/rabbitmq/amqp091-go"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	// TODO: Remove.
-	var send bool
-	flag.BoolVar(&send, "send", false, "send a message to the worker")
-	flag.Parse()
-
-	// TODO: Remove.
-	if send {
-		if err := runSend(); err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
-			os.Exit(1)
-		}
-		os.Exit(0)
-	}
-
 	if err := run(); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
@@ -135,38 +119,4 @@ func retryWaitDuration(retry int) time.Duration {
 	duration += jitter
 
 	return time.Duration(duration)
-}
-
-// TODO: Remove.
-func runSend() error {
-	ctx := context.Background()
-
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	if err != nil {
-		return err
-	}
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare("example", false, false, false, false, nil)
-	if err != nil {
-		return err
-	}
-
-	msg := amqp091.Publishing{
-		ContentType: "text/plain",
-		Body:        []byte("Hello World!"),
-	}
-	err = ch.PublishWithContext(ctx, "", q.Name, false, false, msg)
-	if err != nil {
-		return err
-	}
-	slog.Default().Info("sent", "msg", string(msg.Body))
-
-	return nil
 }
