cdf9d996420b376ffdcf795166168c687231acbb
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-12-19 18:25:51 +0300

    Add worker main part that creates db and s3 and passes it to Worker

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"os"
+
+	"github.com/k11v/brick/internal/run/runpg"
+	"github.com/k11v/brick/internal/run/runs3"
 )
 
 func main() {
-	worker := &Worker{}
+	run := func() int {
+		ctx := context.Background()
 
-	slog.Info("starting worker")
-	if err := worker.Run(); err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
+		db, err := runpg.NewPool(ctx, "postgres://postgres:postgres@127.0.0.1:5432/postgres")
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			return 1
+		}
+		defer db.Close()
+
+		s3 := runs3.NewClient("http://minioadmin:minioadmin@127.0.0.1:9000")
+		worker := &Worker{DB: db, S3: s3}
+
+		slog.Info("starting worker")
+		err = worker.Run()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			return 1
+		}
+
+		return 0
 	}
-	os.Exit(0)
+	os.Exit(run())
 }
