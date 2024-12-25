a0cee13e1290403878fc23f54de0b7afa29f5054
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-12-25 23:23:58 +0300

    Add Handler methods HeaderFromSignIn and HeaderFromSignOut

--- internal/run/runpg/migrations/0_init.down.sql
+++ internal/run/runpg/migrations/0_init.down.sql
@@ -1,12 +1,12 @@
 BEGIN;
 
-DROP TABLE IF EXISTS revoked_access_tokens;
+DROP TABLE IF EXISTS revoked_tokens;
 
 DROP TABLE IF EXISTS user_locks;
 
-DROP TABLE IF EXISTS operation_input_files;
+DROP TABLE IF EXISTS build_input_files;
 
-DROP TABLE IF EXISTS operations;
+DROP TABLE IF EXISTS builds;
 
 DROP EXTENSION IF EXISTS "uuid-ossp";
 
