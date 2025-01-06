d689529d79ee76cf1bb9fbe011dac4dfc1615641
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-01-06 21:29:12 +0500

    Add runner main part with input tar extraction

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/tar"
 	"errors"
 	"flag"
 	"fmt"
@@ -13,21 +14,59 @@ var boundary = flag.String("boundary", "", "set the boundary of multipart conten
 
 func main() {
 	flag.Parse()
+
 	if *boundary != "" {
 		_, _ = fmt.Fprintln(os.Stderr, "empty -boundary flag")
-		os.Exit(2)
+		os.Exit(1)
 	}
 
 	mr := multipart.NewReader(os.Stdin, *boundary)
-	inputTar, err := mr.NextPart()
+
+	tarPart, err := mr.NextPart()
 	if err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(2)
+		os.Exit(1)
 	}
+	tr := tar.NewReader(tarPart)
+	for {
+		var h *tar.Header
+		h, err = tr.Next()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			_, _ = fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		switch h.Typeflag {
+		case tar.TypeReg:
+			var f *os.File
+			f, err = os.Create(h.Name)
+			if err != nil {
+				_, _ = fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+			_, err = io.Copy(f, tarPart)
+			if err != nil {
+				_, _ = fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+			_ = f.Close()
+		case tar.TypeDir:
+			err = os.Mkdir(h.Name, 0o777)
+			if err != nil {
+				_, _ = fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+		default:
+			_, _ = fmt.Fprintln(os.Stderr, "unsupported tar entry type")
+			os.Exit(1)
+		}
+	}
+
 	_, err = mr.NextPart()
 	if !errors.Is(err, io.EOF) {
 		_, _ = fmt.Fprintln(os.Stderr, "extra stdin part")
-		os.Exit(2)
+		os.Exit(1)
 	}
-	_ = inputTar
 }
