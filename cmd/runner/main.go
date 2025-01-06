ae930083274fbb79b5e6c119d5cde32a27600c5c
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-01-06 20:58:30 +0500

    Add runner main part with multipart stdin

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"os"
+)
+
+var boundary = flag.String("boundary", "", "set the boundary of multipart content (required)")
+
 func main() {
+	flag.Parse()
+	if *boundary != "" {
+		_, _ = fmt.Fprintln(os.Stderr, "empty -boundary flag")
+		os.Exit(2)
+	}
+
+	mr := multipart.NewReader(os.Stdin, *boundary)
+	inputTar, err := mr.NextPart()
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(2)
+	}
+	_, err = mr.NextPart()
+	if !errors.Is(err, io.EOF) {
+		_, _ = fmt.Fprintln(os.Stderr, "extra stdin part")
+		os.Exit(2)
+	}
+	_ = inputTar
 }
