c06003a5b2ea9d1f1a8033061e187fee45f24827
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-01-06 23:29:29 +0500

    Add runner main part with run func

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -18,87 +18,90 @@ var (
 )
 
 func main() {
-	flag.Parse()
+	run := func() int {
+		flag.Parse()
 
-	const flagBoundary = "-b"
-	if *boundary != "" {
-		_, _ = fmt.Fprintf(os.Stderr, "error: empty %s flag\n", flagBoundary)
-		os.Exit(2)
-	}
+		const flagBoundary = "-b"
+		if *boundary != "" {
+			_, _ = fmt.Fprintf(os.Stderr, "error: empty %s flag\n", flagBoundary)
+			return 2
+		}
 
-	const flagInputFile = "-i"
-	if *inputFile == "" {
-		_, _ = fmt.Fprintf(os.Stderr, "error: empty %s flag\n", flagInputFile)
-		os.Exit(2)
-	}
-	if *inputFile != "main.md" { // build.Run can only build main.md for now.
-		_, _ = fmt.Fprintf(os.Stderr, "error: %s flag is not %q\n", flagInputFile, "main.md")
-		os.Exit(2)
-	}
+		const flagInputFile = "-i"
+		if *inputFile == "" {
+			_, _ = fmt.Fprintf(os.Stderr, "error: empty %s flag\n", flagInputFile)
+			return 2
+		}
+		if *inputFile != "main.md" { // build.Run can only build main.md for now.
+			_, _ = fmt.Fprintf(os.Stderr, "error: %s flag is not %q\n", flagInputFile, "main.md")
+			return 2
+		}
 
-	const flagOutputFile = "-o"
-	if *outputFile == "" {
-		_, _ = fmt.Fprintf(os.Stderr, "error: empty %s flag\n", flagOutputFile)
-		os.Exit(2)
-	}
+		const flagOutputFile = "-o"
+		if *outputFile == "" {
+			_, _ = fmt.Fprintf(os.Stderr, "error: empty %s flag\n", flagOutputFile)
+			return 2
+		}
 
-	const flagCacheDir = "-c"
-	if *cacheDir == "" {
-		_, _ = fmt.Fprintf(os.Stderr, "error: empty %s flag\n", flagCacheDir)
-		os.Exit(2)
-	}
+		const flagCacheDir = "-c"
+		if *cacheDir == "" {
+			_, _ = fmt.Fprintf(os.Stderr, "error: empty %s flag\n", flagCacheDir)
+			return 2
+		}
 
-	// Read a tar file with input files from stdin
-	// and extract it to the working directory.
-	mr := multipart.NewReader(os.Stdin, *boundary)
-	tarFile, err := mr.NextPart()
-	if err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-	tr := tar.NewReader(tarFile)
-	for {
-		var h *tar.Header
-		h, err = tr.Next()
+		// Read a tar file with input files from stdin
+		// and extract it to the working directory.
+		mr := multipart.NewReader(os.Stdin, *boundary)
+		tarFile, err := mr.NextPart()
 		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
 			_, _ = fmt.Fprintln(os.Stderr, err.Error())
-			os.Exit(1)
+			return 1
 		}
-		switch h.Typeflag {
-		case tar.TypeReg:
-			var f *os.File
-			f, err = os.Create(h.Name)
+		tr := tar.NewReader(tarFile)
+		for {
+			var h *tar.Header
+			h, err = tr.Next()
 			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
 				_, _ = fmt.Fprintln(os.Stderr, err.Error())
-				os.Exit(1)
+				return 1
 			}
-			_, err = io.Copy(f, tarFile)
-			if err != nil {
-				_, _ = fmt.Fprintln(os.Stderr, err.Error())
-				os.Exit(1)
+			switch h.Typeflag {
+			case tar.TypeReg:
+				var f *os.File
+				f, err = os.Create(h.Name)
+				if err != nil {
+					_, _ = fmt.Fprintln(os.Stderr, err.Error())
+					return 1
+				}
+				_, err = io.Copy(f, tarFile)
+				if err != nil {
+					_, _ = fmt.Fprintln(os.Stderr, err.Error())
+					return 1
+				}
+				_ = f.Close()
+			case tar.TypeDir:
+				err = os.Mkdir(h.Name, 0o777)
+				if err != nil {
+					_, _ = fmt.Fprintln(os.Stderr, err.Error())
+					return 1
+				}
+			default:
+				_, _ = fmt.Fprintln(os.Stderr, "error: unsupported tar entry type")
+				return 1
 			}
-			_ = f.Close()
-		case tar.TypeDir:
-			err = os.Mkdir(h.Name, 0o777)
-			if err != nil {
-				_, _ = fmt.Fprintln(os.Stderr, err.Error())
-				os.Exit(1)
-			}
-		default:
-			_, _ = fmt.Fprintln(os.Stderr, "error: unsupported tar entry type")
-			os.Exit(1)
 		}
-	}
-	_, err = mr.NextPart()
-	if !errors.Is(err, io.EOF) {
-		_, _ = fmt.Fprintln(os.Stderr, "error: extra stdin part")
-		os.Exit(1)
-	}
+		_, err = mr.NextPart()
+		if !errors.Is(err, io.EOF) {
+			_, _ = fmt.Fprintln(os.Stderr, "error: extra stdin part")
+			return 1
+		}
 
-	fmt.Println("done")
+		fmt.Println("done")
 
-	os.Exit(0)
+		return 0
+	}
+	os.Exit(run())
 }
