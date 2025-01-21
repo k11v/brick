36672747bef702345e3cef9e56856b5ac453c354
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-01-22 01:30:58 +0300

    Add Handler func decls

--- cmd/server/handler.go
+++ cmd/server/handler.go
@@ -124,169 +124,6 @@ func (h *Handler) Page(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(page)
 }
 
-type ExecuteMainParams struct {
-	Build *build.Build
-	Files []*build.InputFile
-}
-
-func (h *Handler) MainFromBuildButtonClick(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-
-	// Header X-Idempotency-Key.
-	const headerIdempotencyKey = "X-Idempotency-Key"
-	idempotencyKeyHeader := r.Header.Get(headerIdempotencyKey)
-	if idempotencyKeyHeader == "" {
-		h.serveError(w, r, fmt.Errorf("%s header empty or missing", headerIdempotencyKey))
-		return
-	}
-	idempotencyKey, err := uuid.Parse(idempotencyKeyHeader)
-	if err != nil {
-		h.serveError(w, r, fmt.Errorf("%s header: %w", headerIdempotencyKey, err))
-		return
-	}
-
-	// Cookie access_token.
-	const cookieAccessToken = "access_token"
-	accessTokenCookie, err := r.Cookie(cookieAccessToken)
-	if err != nil {
-		h.serveError(w, r, fmt.Errorf("%s cookie: %w", cookieAccessToken, err))
-		return
-	}
-	accessToken, err := parseAndValidateTokenFromCookie(ctx, h.db, h.jwtVerificationKey, accessTokenCookie)
-	if err != nil {
-		h.serveError(w, r, fmt.Errorf("%s cookie: %w", cookieAccessToken, err))
-		return
-	}
-	userID := accessToken.UserID
-
-	mr, err := r.MultipartReader()
-	if err != nil {
-		h.serveError(w, r, fmt.Errorf("request: %w", err))
-		return
-	}
-
-	var (
-		bufPart  *multipart.Part
-		bufErr   error
-		bufNext  = false
-		bufEmpty = true
-	)
-	nextPart := func() (*multipart.Part, error) {
-		if bufNext {
-			bufNext = false
-			return bufPart, bufErr
-		}
-		bufPart, bufErr = mr.NextPart()
-		bufEmpty = false
-		return bufPart, bufErr
-	}
-	unnextPart := func() error {
-		if bufNext || bufEmpty {
-			return errors.New("unnextPart buf next or empty")
-		}
-		bufNext = true
-		return nil
-	}
-
-	var files iter.Seq2[*build.File, error] = func(yield func(*build.File, error) bool) {
-		for i := 0; ; i++ {
-			var (
-				name string
-				typ  string
-				data io.Reader
-			)
-
-		PartLoop:
-			for {
-				part, err := nextPart()
-				if err != nil {
-					if err == io.EOF {
-						break
-					}
-					_ = yield(nil, err)
-					return
-				}
-
-				formName := part.FormName()
-				switch {
-				// Form value files/*/name.
-				case formName == fmt.Sprintf("files/%d/name", i):
-					valueBytes, err := io.ReadAll(part)
-					if err != nil {
-						_ = yield(nil, fmt.Errorf("%s form value: %w", formName, err))
-						return
-					}
-					name = string(valueBytes)
-				// Form value files/*/type.
-				case formName == fmt.Sprintf("files/%d/type", i):
-					valueBytes, err := io.ReadAll(part)
-					if err != nil {
-						_ = yield(nil, fmt.Errorf("%s form value: %w", formName, err))
-						return
-					}
-					typ = string(valueBytes)
-				// Form value files/*/data.
-				case formName == fmt.Sprintf("files/%d/data", i):
-					data = part
-					break PartLoop
-				case strings.HasPrefix(formName, fmt.Sprintf("files/%d/", i+1)):
-					err := unnextPart()
-					if err != nil {
-						_ = yield(nil, err)
-						return
-					}
-					break PartLoop
-				default:
-					_ = yield(nil, fmt.Errorf("%s form value unknown or misplaced", formName))
-					return
-				}
-			}
-
-			file := &build.File{
-				Name: name,
-				Type: typ,
-				Data: data,
-			}
-			if !yield(file, nil) {
-				return
-			}
-		}
-	}
-
-	creator := &build.Creator{DB: h.db, MQ: h.mq, S3: h.s3, BuildsAllowed: 10}
-	createdBuild, err := creator.Create(ctx, &build.CreatorCreateParams{
-		UserID:         userID,
-		Files:          files,
-		IdempotencyKey: idempotencyKey,
-	})
-	if err != nil {
-		h.serveError(w, r, err)
-		return
-	}
-
-	getter := &build.Getter{DB: h.db, S3: h.s3}
-	gotFiles, err := getter.GetInputFiles(ctx, &build.GetterGetParams{
-		ID:     createdBuild.ID,
-		UserID: userID,
-	})
-	if err != nil {
-		h.serveError(w, r, err)
-		return
-	}
-
-	comp, err := h.execute("build_main", &ExecuteMainParams{
-		Build: createdBuild,
-		Files: gotFiles,
-	})
-	if err != nil {
-		h.serveError(w, r, err)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write(comp)
-}
-
 func (h *Handler) DocumentFromChange(w http.ResponseWriter, r *http.Request) {
 	mr, err := r.MultipartReader()
 	if err != nil {
@@ -449,6 +286,193 @@ func (h *Handler) DocumentFromChange(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write(comp)
+}
+
+type ExecuteMainParams struct {
+	Build *build.Build
+	Files []*build.InputFile
+}
+
+// MainFromBuildButtonClick.
+// For status code 400, it responds with build_error.
+func (h *Handler) MainFromBuildButtonClick(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	// Header X-Idempotency-Key.
+	const headerIdempotencyKey = "X-Idempotency-Key"
+	idempotencyKeyHeader := r.Header.Get(headerIdempotencyKey)
+	if idempotencyKeyHeader == "" {
+		h.serveError(w, r, fmt.Errorf("%s header empty or missing", headerIdempotencyKey))
+		return
+	}
+	idempotencyKey, err := uuid.Parse(idempotencyKeyHeader)
+	if err != nil {
+		h.serveError(w, r, fmt.Errorf("%s header: %w", headerIdempotencyKey, err))
+		return
+	}
+
+	// Cookie access_token.
+	const cookieAccessToken = "access_token"
+	accessTokenCookie, err := r.Cookie(cookieAccessToken)
+	if err != nil {
+		h.serveError(w, r, fmt.Errorf("%s cookie: %w", cookieAccessToken, err))
+		return
+	}
+	accessToken, err := parseAndValidateTokenFromCookie(ctx, h.db, h.jwtVerificationKey, accessTokenCookie)
+	if err != nil {
+		h.serveError(w, r, fmt.Errorf("%s cookie: %w", cookieAccessToken, err))
+		return
+	}
+	userID := accessToken.UserID
+
+	mr, err := r.MultipartReader()
+	if err != nil {
+		h.serveError(w, r, fmt.Errorf("request: %w", err))
+		return
+	}
+
+	var (
+		bufPart  *multipart.Part
+		bufErr   error
+		bufNext  = false
+		bufEmpty = true
+	)
+	nextPart := func() (*multipart.Part, error) {
+		if bufNext {
+			bufNext = false
+			return bufPart, bufErr
+		}
+		bufPart, bufErr = mr.NextPart()
+		bufEmpty = false
+		return bufPart, bufErr
+	}
+	unnextPart := func() error {
+		if bufNext || bufEmpty {
+			return errors.New("unnextPart buf next or empty")
+		}
+		bufNext = true
+		return nil
+	}
+
+	var files iter.Seq2[*build.File, error] = func(yield func(*build.File, error) bool) {
+		for i := 0; ; i++ {
+			var (
+				name string
+				typ  string
+				data io.Reader
+			)
+
+		PartLoop:
+			for {
+				part, err := nextPart()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					_ = yield(nil, err)
+					return
+				}
+
+				formName := part.FormName()
+				switch {
+				// Form value files/*/name.
+				case formName == fmt.Sprintf("files/%d/name", i):
+					valueBytes, err := io.ReadAll(part)
+					if err != nil {
+						_ = yield(nil, fmt.Errorf("%s form value: %w", formName, err))
+						return
+					}
+					name = string(valueBytes)
+				// Form value files/*/type.
+				case formName == fmt.Sprintf("files/%d/type", i):
+					valueBytes, err := io.ReadAll(part)
+					if err != nil {
+						_ = yield(nil, fmt.Errorf("%s form value: %w", formName, err))
+						return
+					}
+					typ = string(valueBytes)
+				// Form value files/*/data.
+				case formName == fmt.Sprintf("files/%d/data", i):
+					data = part
+					break PartLoop
+				case strings.HasPrefix(formName, fmt.Sprintf("files/%d/", i+1)):
+					err := unnextPart()
+					if err != nil {
+						_ = yield(nil, err)
+						return
+					}
+					break PartLoop
+				default:
+					_ = yield(nil, fmt.Errorf("%s form value unknown or misplaced", formName))
+					return
+				}
+			}
+
+			file := &build.File{
+				Name: name,
+				Type: typ,
+				Data: data,
+			}
+			if !yield(file, nil) {
+				return
+			}
+		}
+	}
+
+	creator := &build.Creator{DB: h.db, MQ: h.mq, S3: h.s3, BuildsAllowed: 10}
+	createdBuild, err := creator.Create(ctx, &build.CreatorCreateParams{
+		UserID:         userID,
+		Files:          files,
+		IdempotencyKey: idempotencyKey,
+	})
+	if err != nil {
+		h.serveError(w, r, err)
+		return
+	}
+
+	getter := &build.Getter{DB: h.db, S3: h.s3}
+	gotFiles, err := getter.GetInputFiles(ctx, &build.GetterGetParams{
+		ID:     createdBuild.ID,
+		UserID: userID,
+	})
+	if err != nil {
+		h.serveError(w, r, err)
+		return
+	}
+
+	comp, err := h.execute("build_main", &ExecuteMainParams{
+		Build: createdBuild,
+		Files: gotFiles,
+	})
+	if err != nil {
+		h.serveError(w, r, err)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(comp)
+}
+
+func (h *Handler) DocumentFromClearButtonClick(w http.ResponseWriter, r *http.Request) {
+	panic("not implemented")
+}
+
+func (h *Handler) Main(w http.ResponseWriter, r *http.Request) {
+	panic("not implemented")
+}
+
+func (h *Handler) MainFromCancelButtonClick(w http.ResponseWriter, r *http.Request) {
+	panic("not implemented")
+}
+
+// MainFromNewButtonClick responds with build_main for a new build
+// and cancels the current build if it is still cancelable.
+func (h *Handler) MainFromNewButtonClick(w http.ResponseWriter, r *http.Request) {
+	panic("not implemented")
+}
+
+func (h *Handler) OutputFileFromDownloadButtonClick(w http.ResponseWriter, r *http.Request) {
+	panic("not implemented")
 }
 
 func (h *Handler) NotFoundPage(w http.ResponseWriter, r *http.Request) {
