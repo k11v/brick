b5d763800851724fa7f9b55aa47fb76ebe442f51
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-06 16:47:07 +0300

    Remove cmd/server

--- cmd/server/handler_clear_button.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "net/http"
-
-func (h *Handler) ClearButtonClickToFiles(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented")
-}
--- cmd/server/handler_new_button.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "net/http"
-
-// NewButtonClickToMain responds with build_main for a new build
-// and cancels the current build if it is still cancelable.
-func (h *Handler) NewButtonClickToMain(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented")
-}
