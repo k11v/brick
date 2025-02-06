a6c43e113d1dedebb266884f86f7e85881dfbeb0
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-06 17:02:34 +0300

    Move internal/run/runs3 to internal/apps3

--- internal/build/doer.go
+++ internal/build/doer.go
@@ -22,7 +22,7 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 
-	"github.com/k11v/brick/internal/run/runs3"
+	"github.com/k11v/brick/internal/apps3"
 )
 
 var ErrNotFound = errors.New("not found")
@@ -608,7 +608,7 @@ func downloadData(ctx context.Context, s3Client *s3.Client, w io.Writer, key str
 
 	// fakeWriterAt needs manager.Downloader.Concurrency set to 1.
 	_, err := downloader.Download(ctx, fakeWriterAt{w}, &s3.GetObjectInput{
-		Bucket: &runs3.BucketName,
+		Bucket: &apps3.BucketName,
 		Key:    &key,
 	})
 	if err != nil {
