66596c4fe318a166cc1c54f9cc06ed8d3d0af021
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-08 01:01:17 +0300

    Add NewServer mux

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -15,11 +15,14 @@ import (
 func NewServer(db *pgxpool.Pool, mq *app.AMQPClient, st *s3.Client, cfg *Config) (*http.Server, error) {
 	addr := net.JoinHostPort(cfg.Host, strconv.Itoa(cfg.Port))
 
+	mux := http.NewServeMux()
+
 	subLogger := slog.With("source", "http")
 	subLogLogger := slog.NewLogLogger(subLogger.Handler(), slog.LevelError)
 
 	return &http.Server{
 		Addr:     addr,
+		Handler:  mux,
 		ErrorLog: subLogLogger,
 	}, nil
 }
